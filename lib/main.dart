542c48a8cbf8ab4b1fc742d8b02735b92cd14dbf
Ashbee <ksb.forest@gmail.com>
2023-01-22 00:47:13 +0900

    study: Xylophone
    * play multiple sounds
    * update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,8 +8,39 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              xylophoneBtn(Colors.red, 'note1.wav'),
+              xylophoneBtn(Colors.orange, 'note2.wav'),
+              xylophoneBtn(Colors.yellow, 'note3.wav'),
+              xylophoneBtn(Colors.green, 'note4.wav'),
+              xylophoneBtn(Colors.teal, 'note5.wav'),
+              xylophoneBtn(Colors.blue, 'note6.wav'),
+              xylophoneBtn(Colors.purple, 'note7.wav'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void playSound(String audioPath) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource(audioPath));
+  }
+
+  Widget xylophoneBtn(Color color, String audioPath) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(audioPath);
+        },
+        child: null,
+        style: TextButton.styleFrom(
+          backgroundColor: color,
         ),
       ),
     );
